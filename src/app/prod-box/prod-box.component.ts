c86f30873fb487ac5775b1e7a561881b6aa6e91a
Mustafa Mwafy <mwafy092@gmail.com>
2024-12-29 14:44:26 +0200

    asdaa

--- src/app/prod-box/prod-box.component.ts
+++ src/app/prod-box/prod-box.component.ts
@@ -17,41 +17,17 @@ export class ProdBoxComponent {
     });
   }
   handleFaceBookSharing = () => {
-    const urlToScrape = 'https://hesham99.netlify.app/prod'; // Replace with the URL you want to rescrape
-
-    // Facebook Sharing Debugger API URL
-    const debuggerUrl = `https://graph.facebook.com/?id=${encodeURIComponent(
-      urlToScrape
-    )}`;
-
-    fetch(debuggerUrl)
-      .then((response) => response.json())
-      .then((data) => {
-        console.log('Scraping result:', data);
-      })
-      .catch((error) => {
-        console.error('Error scraping URL:', error);
-      });
-    window.FB.api(
-      'https://graph.facebook.com?id=' +
-        'https://hesham99.netlify.app/prod' +
-        '&scrape=true',
-      'post',
+    window.FB.ui(
+      {
+        method: 'share',
+        href: 'https://hesham99.netlify.app/prod', // The URL to share
+      },
       function (response) {
-        console.log('rescrape', response);
-        window.FB.ui(
-          {
-            method: 'share',
-            href: 'https://hesham99.netlify.app/prod', // The URL to share
-          },
-          function (response) {
-            if (response && !response.error_message) {
-              alert('Post shared successfully!');
-            } else {
-              alert('Error while sharing.');
-            }
-          }
-        );
+        if (response && !response.error_message) {
+          alert('Post shared successfully!');
+        } else {
+          alert('Error while sharing.');
+        }
       }
     );
   };
