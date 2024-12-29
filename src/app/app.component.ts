887ffd6934717d35c1058cbb7e5c584ac98ae94e
Mustafa Mwafy <mwafy092@gmail.com>
2024-12-29 13:43:30 +0200

    asdaa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -62,30 +62,28 @@ export class AppComponent {
   };
 
   handleFaceBookSharing = () => {
-    window.FB.Event.subscribe('xfbml.render', function () {
-      window.FB.api(
-        'https://graph.facebook.com?id=' +
-          'https://hesham99.netlify.app' +
-          '&scrape=true',
-        'post',
-        function (response) {
-          console.log('rescrape', response);
-          window.FB.ui(
-            {
-              method: 'share',
-              href: 'https://hesham99.netlify.app/', // The URL to share
-            },
-            function (response) {
-              if (response && !response.error_message) {
-                alert('Post shared successfully!');
-              } else {
-                alert('Error while sharing.');
-              }
+    window.FB.api(
+      'https://graph.facebook.com?id=' +
+        'https://hesham99.netlify.app' +
+        '&scrape=true',
+      'post',
+      function (response) {
+        console.log('rescrape', response);
+        window.FB.ui(
+          {
+            method: 'share',
+            href: 'https://hesham99.netlify.app/', // The URL to share
+          },
+          function (response) {
+            if (response && !response.error_message) {
+              alert('Post shared successfully!');
+            } else {
+              alert('Error while sharing.');
             }
-          );
-        }
-      );
-    });
+          }
+        );
+      }
+    );
   };
 
   initFacebook = async () => {
